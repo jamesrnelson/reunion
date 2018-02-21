b9cfc989361909bc70f49fc4792f16ef48dfb2dc
JNelson <nelson.jimmy@gmail.com>
2018-02-21 09:32:05 -0700

    working on adding participants

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,15 @@
+# Defines the activity class
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, amount_paid)
+    new_participants = { name => amount_paid }
+    @participants.merge(new_participants)
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new('Hiking')
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_the_activity_has_a_name
+    activity = Activity.new('Hiking')
+
+    assert_equal 'Hiking', activity.name
+  end
+
+  def test_can_add_participant
+    activity = Activity.new('Hiking')
+
+    assert_empty activity.participants
+
+    activity.add_participant('Alistair', 40)
+    activity.add_participant('Owen', 30)
+
+    assert_instance_of Hash, activity.participants
+  end
+
+  def test_the_hash_tells_how_much_participant_paid
+    activity = Activity.new('Hiking')
+    activity.add_participant('Alistair', 40)
+    activity.add_participant('Owen', 30)
+
+    assert_equal 40, activity.participants['Alistair']
+  end
 end
